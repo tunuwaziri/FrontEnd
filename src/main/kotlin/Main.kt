72bfa3d40baece16eb7e243396a0b8b7b288fbf5
tunuwaziri <tunuwazirii@gmail.com>
2024-02-08 07:27:11 +0300

    First Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var fullName = "Janet William"
+    println(fullName)
+    var age = 18
+    println(age)
+    var weight= 56
+    println(weight)
+    var citizen = "Kenyan"
+    println(citizen)
+    var phoneNumber ="0715684560"
+    println(phoneNumber)
+
+
+
+}
